55038f5ceeff6fc5d61f1a3c293d8866b6866d95
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-09-24 14:47:21 -0400

    Upload counts directly to Drive and greatly speed up API accesses.

--- counts.py
+++ counts.py
@@ -21,29 +21,61 @@ def load_workspace():
             workspace = workspace_attrs
     return workspace
 
-def load_projects(workspace, team=None):
-    projects = {}
-    for project_attrs in api.list_projects(workspace['id'], include_archived=False):
-        project = api.get_project(project_attrs['id'])
-        if team is None or project['team']['name'] == team:
-            projects[project['id']] = project
+def list_projects(workspace, include_archived=False, filters=None):
+    if include_archived:
+        include_archived = "true"
+    else:
+        include_archived = "false"
+    target = "projects?archived=%s" % (include_archived)
+    if workspace:
+        target = "workspaces/%d/" % (workspace) + target
+    if filters:
+        target += "&opt_fields=%s" % build_filter_for_fields(filters)
+    return api._asana(target)
+
+def load_team(workspace, team_name=None):
+    for team in api.organization_teams(workspace['id']):
+        if team['name'] == team_name:
+            return team
+
+def load_projects(workspace, team_name=None):
+    projects = []
+    team = load_team(workspace, team_name)
+    for project in list_projects(workspace['id'], include_archived=False,
+            filters=['name', 'team']):
+        if team is None or project['team']['id'] == team['id']:
+            projects.append(project)
     return projects
 
-def load_tasks_for_project(project):
-    tasks_data = api.get_project_tasks(project['id'])
-    tasks = {}
-    for task_attrs in tasks_data:
-        task_id = task_attrs['id']
-        tasks[task_id] = api.get_task(task_id)
-    return tasks
+def get_project_tasks(project, filters=None, include_archived=False):
+    if include_archived:
+        include_archived = "true"
+    else:
+        include_archived = "false"
+    target = 'projects/%d/tasks?include_archived=%s'
+    if filters:
+        target += "&opt_fields=%s" % build_filter_for_fields(filters)
+    return api._asana(target % (project['id'], include_archived))
+
+def load_tasks_for_project(project, full=False):
+    if full:
+        tasks_data = api.get_project_tasks(project['id'])
+        tasks = {}
+        for task_attrs in tasks_data:
+            task_id = task_attrs['id']
+            tasks[task_id] = api.get_task(task_id)
+        return tasks
+    else:
+        return get_project_tasks(project,
+                filters=['completed', 'completed_at', 'created_at'])
 
 def count_tasks(tasks):
     projects = load_enriched_projects()
 
     total_tasks = 0
     total_open_tasks = 0
-    for project in projects.values():
-        tasks = project['tasks'].values()
+    for project in projects:
+        tasks = project['tasks']
         total_tasks += len(tasks)
         open_tasks = [task for task in tasks if not task['completed']]
         total_open_tasks += len(open_tasks)
@@ -62,7 +94,7 @@ def filter_tasks(tasks, before_date=None, after_date=None):
 def load_enriched_projects():
     workspace = load_workspace();
     projects = load_projects(workspace, TEAM)
-    for project in projects.values():
+    for project in projects:
         project['tasks'] = load_tasks_for_project(project)
     return projects
 
@@ -76,15 +108,12 @@ def calculate_burnup(since_days_ago=14):
     for date in dates:
         counts[date]['total'] = 0
         counts[date]['closed'] = 0
-        for project in projects.values():
+        for project in projects:
             # Make sure to create a list here, otherwise the generator will be
             # done when you call matching_tasks again to get closed counts
-            matching_tasks = list(filter_tasks(project.get('tasks', {}).values(), date))
+            matching_tasks = list(filter_tasks(project.get('tasks', {}), date))
             counts[date]['total'] += len(matching_tasks)
             counts[date]['closed'] += len([task for task in matching_tasks if task['completed']])
-
-    for date in sorted(counts.keys()):
-        print("%s,%d,%d" % (date.isoformat(), counts[date]['total'], counts[date]['closed']))
     return counts
 
 def load_tag(workspace, tag_name):
@@ -98,46 +127,49 @@ def get_task_lazy(task_id):
         all_tasks[task_id] = api.get_task(task_id)
     return all_tasks[task_id]
 
+def build_filter_for_fields(filters):
+    fkeys = [x.strip().lower() for x in filters]
+    fields = ",".join(fkeys)
+    return fields
+
+def get_tag_tasks(tag, filters=None):
+    target = 'tags/%d/tasks'
+    if filters:
+        target += "?opt_fields=%s" % build_filter_for_fields(filters)
+    return api._asana(target % tag['id'])
+
 def calculate_stats():
     workspace = load_workspace()
 
-    bugs = {}
-    for bug_task_attr in api.get_tag_tasks(load_tag(workspace, "Bug")['id']):
-        bugs[bug_task_attr['id']] = get_task_lazy(bug_task_attr['id'])
+    bugs = []
+    bugs = get_tag_tasks(load_tag(workspace, "Bug"),
+            filters=['completed', 'completed_at', 'created_at'])
 
     now = pytz.UTC.localize(datetime.utcnow())
     week_ago = now - timedelta(days=7)
 
-    open_bugs = [bug for bug in bugs.values() if not bug['completed']]
-    opened_in_last_week = [bug for bug in bugs.values()
+    open_bugs = [bug for bug in bugs if not bug['completed']]
+    opened_in_last_week = [bug for bug in bugs
             if not bug['completed'] and dateutil.parser.parse(bug['created_at']) >= week_ago]
 
-    closed_in_last_week = [bug for bug in bugs.values()
+    closed_in_last_week = [bug for bug in bugs
             if bug['completed'] and dateutil.parser.parse(bug['completed_at']) >= week_ago]
 
-    p1 = {}
-    for task_attr in api.get_tag_tasks(load_tag(workspace, "P1")['id']):
-        task = get_task_lazy(task_attr['id'])
-        if not task['completed']:
-            p1[task['id']] = task
-
-    p2 = {}
-    for task_attr in api.get_tag_tasks(load_tag(workspace, "P2")['id']):
-        task = get_task_lazy(task_attr['id'])
-        if not task['completed']:
-            p2[task['id']] = task
-
-    p3 = {}
-    for task_attr in api.get_tag_tasks(load_tag(workspace, "P3")['id']):
-        task = get_task_lazy(task_attr['id'])
-        if not task['completed']:
-            p3[task['id']] = task
+    p1 = [task for task in get_tag_tasks(load_tag(workspace, "P1"),
+                filters=['completed', 'completed_at', 'created_at'])
+            if not task['completed']]
+    p2 = [task for task in get_tag_tasks(load_tag(workspace, "P2"),
+                filters=['completed', 'completed_at', 'created_at'])
+            if not task['completed']]
+    p3 = [task for task in get_tag_tasks(load_tag(workspace, "P3"),
+                filters=['completed', 'completed_at', 'created_at'])
+            if not task['completed']]
 
     projects = load_projects(workspace, TEAM)
     security_tasks = []
     ota_tasks = []
     quick_sync_tasks = []
-    for project in projects.values():
+    for project in projects:
         if project['name'] == "Security":
             security_tasks = api.get_project_tasks(project['id'])
         elif project['name'] == "OTA Firmware Updates":
@@ -148,9 +180,9 @@ def calculate_stats():
     print("Open bugs: %d" % len(open_bugs))
     print("Bugs closed in last 7 days: %d" % len(closed_in_last_week))
     print("Bugs opened in last 7 days: %d" % len(opened_in_last_week))
-    print("Open P1 tasks: %d" % len(p1.values()))
-    print("Open P2 tasks: %d" % len(p2.values()))
-    print("Open P3 tasks: %d" % len(p3.values()))
+    print("Open P1 tasks: %d" % len(p1))
+    print("Open P2 tasks: %d" % len(p2))
+    print("Open P3 tasks: %d" % len(p3))
     print("Open security tasks: %d" % len(security_tasks))
     print("Open OTA tasks: %d" % len(ota_tasks))
     print("Open QuickSync tasks: %d" % len(quick_sync_tasks))
--- record_counts.py
+++ record_counts.py
@@ -1,21 +1,30 @@
-import keen
-from datetime import datetime
+import gdata.spreadsheet.service
+import getpass
 
 from counts import calculate_burnup
 
+BURNUP_CHART_SPREADSHEET_KEY="1_xGPZ81Va06pbcclgW7wqsO-pjR_VlU0ZE9mxyzAbmM"
+DEFAULT_WORKSHEET_ID = 'od6'
+
 def upload_historical(since_days_ago):
     counts = calculate_burnup(since_days_ago=since_days_ago)
-    events = [{"total": counts[date]['total'],
-                    "closed": counts[date]['closed'],
-                    "open": counts[date]['total'] - counts[date]['closed'],
-                    'keen': {
-                        'timestamp': date.isoformat()
-                    }}
-                for date in sorted(counts.keys())]
-    keen.add_events({"task_counts": events})
+    for date in sorted(counts.keys()):
+        # Must specify spreadsheet headers as lowercase, silly Google API
+        row = {"date": date.isoformat(),
+                "total": "%d" % counts[date]['total'],
+                "closed": "%d" % counts[date]['closed']}
+        client.InsertRow(row, BURNUP_CHART_SPREADSHEET_KEY, DEFAULT_WORKSHEET_ID)
+        print("Uploaded data for %s" % date.isoformat())
 
 def upload_today():
-    upload_historical(0)
+    upload_historical(1)
 
 if __name__ == '__main__':
+    client = gdata.spreadsheet.service.SpreadsheetsService()
+    client.debug = False
+    client.email = raw_input("Google Email: ")
+    client.password = getpass.getpass()
+    client.source = 'some description'
+    client.ProgrammaticLogin()
+
     upload_today()
